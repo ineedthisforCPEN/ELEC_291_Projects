5ca2fb62136e79c196ccc760a3d31881608fcae0
Andy <andygbwong@gmail.com>
2016-02-28 18:47:48 -0800

    merged UltrasonicSensorAI with Project1

--- DCMotor/Project1/Project1.ino
+++ DCMotor/Project1/Project1.ino
@@ -1,3 +1,5 @@
+#include <Servo.h>
+
 // Speed constants
 #define MAX_SPEED 255
 #define MIN_SPEED 0 // not moving
@@ -18,18 +20,26 @@
 #define FORWARD 0
 #define RIGHT 1
 #define LEFT 2
+#define BACKWARDS 3
 
-#define ECHO 13
-#define TRIGGER 12
-#define TEMPERATURE A0
+#define ECHO 10         // Arduino pin to which the ultrasonic sensor's echo pin is connected
+#define TRIGGER 11      // Arduino pin to which the ultrasonic sensor's trigger pin is connected
+#define TEMPERATURE A0  // Arduino pin to which the LM35 output is connected
+#define SERVO 12        // Arduino pin to which the servo output is connected
 
 #define SENSOR_F A1
 #define SENSOR_L A2
 #define SENSOR_R A3
 #define BLACK_THRESHOLD 100
 
+#define STOP_THRESHOLD 40.00
+#define SCAN_THRESHOLD 10.00
+#define SLOW_DOWN_TIME 1000
+
+Servo scanningServo;
 bool leave = false;
 int sensorf_value, sensorl_value, sensorr_value = 0;
+int currentServoDegrees = 90;   // Servo is initially facing forwards
 int current_speed = 0;
 
 void setup() {
@@ -47,6 +57,7 @@ void setup() {
   pinMode(SENSOR_F, INPUT);
   pinMode(SENSOR_L, INPUT);
   pinMode(SENSOR_R, INPUT);
+  scanningServo.attach(SERVO);
 }
 
 void loop() {
@@ -64,7 +75,43 @@ void loop() {
 // Executes the first principle function
 
 void prncp_func1() {
+    int obstacleDistance;
+  int i;
+
+  move_forward(MAX_SPEED);
   
+  obstacleDistance = distanceFromSensor();
+  if (obstacleDistance <= STOP_THRESHOLD) {
+    int turnDirection;
+    
+    // Slow down if an object is detected and come to a stop
+    slow_down(SLOW_DOWN_TIME);
+    Serial.println("STOPPED");
+    delay(500);
+    
+    // Scan the surroundings and take appropriate action
+    turnDirection = scanEnvironment();
+    switch(turnDirection) {
+      case LEFT:
+        for (i = 0; i < 300; i++) {
+          turn_robot(LEFT);
+        }
+        Serial.println("TURNING LEFT");
+        break;
+      case RIGHT:
+        for (i = 0; i < 300; i++) {
+          turn_robot(RIGHT);
+        }
+        Serial.println("TURNING RIGHT");
+        break;
+      case BACKWARDS:
+        // Default case is to go backwards
+      default:
+        //goBackwardsFunction();
+        Serial.println("GOING BACKWARDS");
+        break;
+    }
+  }
 }
 
 // Executes the second principle function
@@ -196,23 +243,25 @@ void set_motors(int direction) {
   }
 }
 
-//------------------------------------------------------------------------------------------------------
+//------------------------------------------------
+// FUNCTIONS FOR MEASURING DISTANCE
+//------------------------------------------------
 
 /**
-   This function is meant to calculate the sound's period based on the reading from the
-   LM35 temperature sensor and lecture slides. It reads the temperature sensor value, then
-   returns the calculated period.
-
-   This function takes into account that the sound wave must travel twice the distance
-   (hence, 20000.0 is used instead of 10000.0)
-*/
+ * This function is meant to calculate the sound's period based on the reading from the
+ * LM35 temperature sensor and lecture slides. It reads the temperature sensor value, then
+ * returns the calculated period.
+ * 
+ * This function takes into account that the sound wave must travel twice the distance
+ * (hence, 20000.0 is used instead of 10000.0)
+ */
 float calculatePeriod(void) {
   long startTime = millis();                              // Get current uptime of Arduino board
   long totalMilliVolts = 0;                               // Long for storing total millivolts (long to prevent overflow)
   long numMeasures = 0;                                   // Long for storing number of measurements (long to prevent overflow)
 
   while (millis() - startTime < 50) {                     // Measure for 50 milliseconds
-    totalMilliVolts += analogRead(TEMPERATURE);        // Add to total millivolts measures
+    totalMilliVolts += analogRead(TEMPERATURE);           // Add to total millivolts measures
     numMeasures++;                                        // Increment measure counter
   }
 
@@ -221,16 +270,16 @@ float calculatePeriod(void) {
   // The value 0.415282392 is used instead of 500.0/1024.0 - we believe this will reduce the number of computations needed
   // Calculation is (1000000 / 100) * (1 / (331.5 + 0.6 * Temperature))
   // Our reduced calculation is seen below
-  return 20000.0 / (331.5 + 0.6 * (((float) totalMilliVolts) / ((float) numMeasures) * (0.415282392)));
-  /*--------------------- Temperature in C -----------------------*/
+  return 20000.0 / (331.5 + 0.6*(((float) totalMilliVolts)/((float) numMeasures) * (0.415282392)));
+                                /*--------------------- Temperature in C -----------------------*/
 }
 
 /**
-   This function determines how far away an object is from the sensor. The calculations
-   are based on the formula given in the lecture slides
-
-   Distance in cm = Echo pulse width (in us) / period
-*/
+ * This function determines how far away an object is from the sensor. The calculations
+ * are based on the formula given in the lecture slides
+ * 
+ * Distance in cm = Echo pulse width (in us) / period
+ */
 float distanceFromSensor(void) {
   // Send a start signal to the trigger pin
   digitalWrite(TRIGGER, HIGH);
@@ -321,4 +370,68 @@ void turn_to_check_path(int direction) {
 
 }
 
+//------------------------------------------------
+// FUNCTION FOR SCANNING THE ENVORONMENT
+//------------------------------------------------
 
+/**
+ * This function scans the environment first by rotating
+ * the ultrasonic sensor to face left of the robot's
+ * previous path (90 degrees left), then rotate the sensor
+ * to the right (180 degrees right), then bring the sensor
+ * back to face forwards (90 degrees left). The function
+ * then determines which direction to rotate and returns
+ * the direction's respected integer representation
+ * (i.e. 0 for left, 1 for right). If the robot is stuck
+ * (i.e. not viable to turn left or right), the function
+ * will return BACKWARDS (2).
+ * 
+ * Return: LEFT (0) if robot should turn left
+ *         RIGHT (1) if robot should turn right
+ *         BACKWARDS (2) if robot should go backwards
+ */
+int scanEnvironment(void) {
+  int leftDistance;
+  int rightDistance;
+  
+  // Smoothly turn the sensor to face the left
+  while (currentServoDegrees > 0) {
+    scanningServo.write(--currentServoDegrees);
+    delay(4);
+  }
+
+  // Measure the distance to the nearest object
+  rightDistance = distanceFromSensor();
+  Serial.print("Right:\t");
+  Serial.println(rightDistance);
+
+  // Smoothly turn the sensor to face the right
+  while (currentServoDegrees < 180) {
+    scanningServo.write(++currentServoDegrees);
+    delay(4);
+  }
+
+  // Measure the distance to the nearest object
+  leftDistance = distanceFromSensor();
+  Serial.print("Left:\t");
+  Serial.println(leftDistance);
+
+  // Smoothly return the sensor to face forwards
+  while (currentServoDegrees > 90) {
+    scanningServo.write(--currentServoDegrees);
+  }
+
+  if (leftDistance < SCAN_THRESHOLD && rightDistance < SCAN_THRESHOLD) {
+    // If stuck, move backwards
+    return BACKWARDS;
+  } else if (leftDistance > rightDistance) {
+    // Turn left if there's more space on the left
+    return LEFT;
+  } else {
+    // Turn right if there's more space on the right
+    return RIGHT;
+  }
+  
+  // By default, go backwards
+  return BACKWARDS;
+}
