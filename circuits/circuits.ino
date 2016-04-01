b75c85d480d7405cb1532918ee8dacf8a8c4d007
Tiger <tiger.zhong.turbo@gmail.com>
2016-04-01 15:49:22 -0700

    Tiger adding pushbutton and changing the input pins

--- circuits/circuits.ino
+++ circuits/circuits.ino
@@ -7,9 +7,15 @@
 const int humidityPin = 7;
 const int temperaturePin = A0;
 // Set the RGB LED pins
-const int rPin = 5; 
-const int bPin = 9; 
-const int gPin = 8; 
+const int rPin = 9; 
+const int bPin = 12; 
+const int gPin = 11; 
+
+
+const int buttonPin = 8;     //  pushbutton pin number
+
+int buttonState = 0;         // record the push button status
+
 
 
 /* Initialize the DHT */
@@ -28,6 +34,10 @@ void setup() {
   digitalWrite(rPin, HIGH);
   digitalWrite(bPin, HIGH);
   digitalWrite(gPin, HIGH);
+
+
+  //push button input
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {  
@@ -47,6 +57,21 @@ void loop() {
      flashGreen();  // Flash green on the RGB LED
     else
      flashRed();  // Flash red on the RGB LED
+
+
+
+     
+      // detect if the button is pressed:
+  buttonState = digitalRead(buttonPin);
+
+  // if the button is pressed, turn on voice command
+  if (buttonState == HIGH) {
+    Serial.print ("ON");
+  } 
+  else {
+    Serial.print("OFF");
+  }
+     
   }
 }
 
