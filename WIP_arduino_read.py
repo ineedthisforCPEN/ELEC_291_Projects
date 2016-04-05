2b8d58f5cff89b86f9e735446a8fa98da00adb29
ineedthisforCPEN <ineedthisforCPEN@users.noreply.github.com>
2016-04-04 20:22:27 -0700

    Added files via upload
    Manually adding most recent WIP_arduino_read.py file

--- WIP_arduino_read.py
+++ WIP_arduino_read.py
@@ -44,31 +44,35 @@ from sys import argv, exit
 #--------------------------------------------------
 
 # See if the script is being used properly (requires a second argument)
-if len(argv < 2):
-    exit("Invalid usage of {}".format(argv[0]))
+#if len(argv) < 2:
+#    exit("Invalid usage of {}. Must have one argument between 0 and 255 inclusive".format(argv[0]))
 
 # See if the second argument is valid (must be a number between 0 and 255 - a byte)
-if argv[1] < 0 or argv[1] > 255:
-    exit("Ivalid second argument {}. Must be between 0 and 255 inclusive".format(argv[1]))
+#if int(argv[1]) < 0 or int(argv[1]) > 255:
+#    exit("Ivalid second argument {}. Must be between 0 and 255 inclusive".format(argv[1]))
 
 # Get path to arduino
-listdev = subprocess.Popen(['ls, '/dev/'], stdout = subprocess.PIPE)
+listdev = subprocess.Popen(['ls', '/dev/'], stdout = subprocess.PIPE)
 path = subprocess.check_output(('grep', 'ttyACM'), stdin = listdev.stdout)
 
 # Create serial object for arduino
-ardiuno = serial.Serial("/dev/{}".format(path[:-1]), 9600)
-arduino.write(argv[1])
+ser = serial.Serial('/dev/{}'.format(path[:-1]), 9600)
+sleep(2)
+ser.write(argv[1])
 
 # Wait for arduino to be ready
 sleep(2)
 
 # Read values from arduino
-arduino_output = arduino.readline()
+arduino_output = ser.readline()
 output_list = arduino_output.split('|')
+print(arduino_output)
+print(output_list)
+a_value = ""    # Value for the accelerometer
 b_value = ""    # Value for the button 
 
 # Determine which data should go where
-if argv[1] & 1 and len(output_list) > 0:
+if int(argv[1]) & 1 and len(output_list) > 0:
     try:
         t_file = open('/var/www/wwwdata/temperature.txt', 'w')
     except:
@@ -77,22 +81,16 @@ if argv[1] & 1 and len(output_list) > 0:
     t_file.write(output_list.pop(0))
     t_file.close()
 
-if argv[1] & 2 and len(output_list) > 0:
+if int(argv[1]) & 2 and len(output_list) > 0:
     try:
         h_file = open('/var/www/wwwdata/humidity.txt', 'w')
     except:
         exit('ERROR: Could not open humidity file - make sure you have the appropriate permission')
 
-    h_value = output_list.pop(0)
-
-    while 'n' in h_value or 'a' in h_value:
-        serial.write(2)     # Only want humidity sensor data
-        h_value = serial.readline().split('|')[0]
-
-    h_file.write(h_value)
+    h_file.write(output_list.pop(0))
     h_file.close()
 
-if argv[1] & 4 and len(output_list) > 0:
+if int(argv[1]) & 4 and len(output_list) > 0:
     try:
         u_file = open('/var/www/wwwdata/ultrasonicsensor.txt', 'w')
     except:
@@ -101,21 +99,32 @@ if argv[1] & 4 and len(output_list) > 0:
     u_file.write(output_list.pop(0))
     u_file.close()
 
-if argv[1] & 8 and len(output_list) > 0:
+if int(argv[1]) & 8 and len(output_list) > 0:
     try:
         a_file = open('/var/www/wwwdata/accelerometer.txt', 'w')
     except:
         exit('ERROR: Could not open accelerometer file - make sure you have the appropriate permission')
 
-    a_file.write(output_list.pop(0))
+    a_value = output_list.pop(0)
+    a_file.write(a_value)
     a_file.close()
 
-if argv[1] & 16 and len(output_list) > 0:
+if int(argv[1]) & 16 and len(output_list) > 0:
     b_value = output_list.pop(0)
 
-# Determine if the humidity sensor's readings were valid
-while 'n' in h_value or 'a' in h_value:
-    serial.write(2)     # Only want humidity sensor data
-    h_value = serial.readline().split('|')[0]
+# Check to see if music should be stopped (stop if button read is 1)
+if len(output_list) > 0:
+    music_stop = int(output_list.pop()[:-1])
+    if music_stop: os.system('kill $(pgrep omxplayer)')
 
+# Change the boot config.txt file based on accelerometer value
+if a_value == '4':  # Set screen orientation to be vertical
+    os.system("sudo sed -i '61s/.*/display_rotate=3/' /boot/config.txt")
+else:               # Set screen orientation to be horizontal
+    os.system("sudo sed -i '61s/.*/#display_rotate=3/' /boot/config.txt")
 
+# Based on the value of the push-button, change the orientation of the screen
+# Screen orientation will only change when the raspberry pi reboots
+if b_value == '1':    # Set screen orientation to be vertical
+    rbtfile = open('/var/www/wwwdata/rbtflag', 'w')
+    rbtfile.close()
